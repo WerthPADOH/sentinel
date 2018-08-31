9cb55bde825638fadf7954e314b6b26114ecc3ff
Nathan Werth <nwerth@pa.gov>
2018-08-31 10:11:08 -0400

    Removed leftover code from naaccr package

--- tests/testthat/test-sentineled.R
+++ tests/testthat/test-sentineled.R
@@ -1,8 +1,7 @@
 library(testthat)
 library(naaccr)
 
-
-context("Reading NAACCR-formatted files")
+context("sentineled behavior")
 
 test_that("sentineled stores sentinel levels properly", {
   s <- sentineled(NA, "cat")
@@ -101,25 +100,4 @@ test_that("sentineled vectors are good for use", {
   sub_x <- s[sentinels(s) == "x"]
   expect_identical(as.numeric(sub_x), NA_real_)
   expect_identical(is.na(s), c(FALSE, TRUE, TRUE))
-})
-
-test_that("naaccr_sentineled works", {
-  gleason <- naaccr_sentineled(
-    c("02", "10", "X7", "X8", "X9", ""),
-    "gleasonScoreClinical"
-  )
-  expect_is(gleason, "sentineled")
-  expect_identical(as.numeric(gleason), c(2, 10, NA, NA, NA, NA))
-  expect_identical(
-    levels(gleason),
-    c("", "procedure not done", "not applicable", "not documented")
-  )
-  expect_identical(
-    as.character(sentinels(gleason)),
-    c("", "", "procedure not done", "not applicable", "not documented", NA)
-  )
-})
-
-test_that("naaccr_sentineled warns for non-fields", {
-  expect_warning(naaccr_sentineled(1, "foo"))
 })
